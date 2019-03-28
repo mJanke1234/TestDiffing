33a70a5629f8249fbf0b00474913cd1414f8f134
Mario Janke <mario.janke@tu-ilmenau.de>
2019-03-28 13:46:35 +0100

    running example done

--- runningExample.java
+++ runningExample.java
@@ -2,3 +2,19 @@ package org.drools.reteoo;
 
 import org.drools.FactException;
 import org.drools.spi.PropagationContext;
+
+
+public class LeftInputAdapterNode 
+{
+    private ObjectSource objectSource;
+
+    private int          column;
+
+    public LeftInputAdapterNode(
+                                int column,
+                                ObjectSource source)
+    {
+        this.column = column;
+        this.objectSource = source;
+    }
+}
