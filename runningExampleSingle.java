dc5750d497b19dc0574800804add78434cdf58c8
Mario Janke <mario.janke@tu-ilmenau.de>
2019-03-29 09:01:33 +0100

    running example single ended

--- runningExampleSingle.java
+++ runningExampleSingle.java
@@ -3,3 +3,15 @@ package org.drools.reteoo;
 import org.drools.FactException;
 import org.drools.spi.PropagationContext;
 
+
+public class LeftInputAdapterNode 
+{
+
+    private int column;
+
+    public LeftInputAdapterNode(  int column)
+    {
+        this.column = column;
+
+    }
+}
